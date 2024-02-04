1de56cd59dfecfa4403d1b1842b1feeb138fa05a
chikbob <ricarrdo1488@gmail.com>
2024-02-04 21:57:57 +0300

    add: exe file fixed the bug: the game did not loop

--- python/LR10/LR10_2.py
+++ python/LR10/LR10_2.py
@@ -2,6 +2,7 @@ import turtle
 
 t = turtle.Turtle()
 t.shape('turtle')
+t.speed(0)
 
 colors = ['red', 'orange', 'green', 'blue', 'purple']
 
--- python/SRS1/Hangman.py
+++ python/SRS1/Hangman.py
@@ -9,9 +9,7 @@ def main():
     global already_guessed
     global length
     global play_game
-    words_to_guess = ["вогнегасник", "калинка", "бат", "радість", "кольчуга", "перука", "алтабас", "кульбаба",
-                      "складка", "барометр", "автохром", "градусник", "асфальт", "купа", "сорока", "колгоспник",
-                      "лавочник", "светр", "павич", "коляска"]
+    words_to_guess = ["біт", "радість", "кольчуга", "перука", "барометр", "автохром", "градусник", "купа", "лавочник", "светр", "павич", "коляска"]
     word = random.choice(words_to_guess)
     length = len(word)
     count = 0
@@ -23,11 +21,12 @@ def main():
 def play_loop():
     global play_game
     play_game = input("Ви хочете пограти ще раз? т = так, н = ні \n")
-    if play_game == "т" or play_game == "так" or play_game == "Так" or play_game == "ТАК" or play_game == "y" or play_game == "Y":
+    play_game = play_game.lower()
+    if play_game == "т" or play_game == "так" or play_game == "y" or play_game == "yes":
         main()
+        hangman()
     else:
         print("Дякую за гру!")
-        exit()
 
 
 def hangman():
@@ -42,7 +41,6 @@ def hangman():
     if len(guess.strip()) == 0 or len(guess.strip()) >= 2 or guess <= "9":
         print("Введені дані не вірні! Спробуйте ще раз!\n")
         hangman()
-
 
     elif guess in word:
         already_guessed.extend([guess])
